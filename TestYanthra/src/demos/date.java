4b9b433483a823708363771f70c1af2aa86c9f88
5uthomas <5uthomas1992@gmail.com>
2019-05-27 23:49:43 +0530

    commit to master 2

--- TestYanthra/src/demos/date.java
+++ TestYanthra/src/demos/date.java
@@ -6,8 +6,8 @@ public class date
 {
 	public static void main(String[] args) 
 	{
-		Date d=new Date();
-		String s=d.toString();
+		//Date d=new Date();
+		//String s=d.toString();
 		System.out.println(s);
 		String s2=s.replaceAll(":", "_");
 		System.out.println(s2);
